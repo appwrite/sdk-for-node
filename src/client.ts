7ab342fbac4efbb1683061d38a5434be5269b4df
root <chiragaggarwal5k@gmail.com>
2025-07-02 05:26:03 +0000

    chore: add changelog

--- src/client.ts
+++ src/client.ts
@@ -33,7 +33,7 @@ class AppwriteException extends Error {
 }
 
 function getUserAgent() {
-    let ua = 'AppwriteNodeJSSDK/17.0.0';
+    let ua = 'AppwriteNodeJSSDK/17.1.0';
 
     // `process` is a global in Node.js, but not fully available in all runtimes.
     const platform: string[] = [];
@@ -82,7 +82,7 @@ class Client {
         'x-sdk-name': 'Node.js',
         'x-sdk-platform': 'server',
         'x-sdk-language': 'nodejs',
-        'x-sdk-version': '17.0.0',
+        'x-sdk-version': '17.1.0',
         'user-agent' : getUserAgent(),
         'X-Appwrite-Response-Format': '1.7.0',
     };
